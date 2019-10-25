3e65405f254ff6712ca5621cc0434c1e81d16989
Zijian Huang <lhq8722861@gmail.com>
2019-10-24 20:56:31 -0400

    111

--- src/GameService.java
+++ src/GameService.java
@@ -6,26 +6,42 @@ import java.util.ArrayList;
 
 public class GameService {
     private DatabaseConnectionService dbService = null;
+    private ArrayList<Float> lowest_value;
     public GameService(DatabaseConnectionService dbService){
         this.dbService  = dbService;
+        this.lowest_value = new ArrayList<Float>();
     }
 
+    public ArrayList<Float> getValue(){
+        return this.lowest_value;
+    }
+
+
     public boolean getLowValue(String input) throws SQLException {
-        CallableStatement cs =this.dbService.getConnection().prepareCall("{?=call get_lowest_highest_price(?)}");
-        cs.registerOutParameter(1, Types.INTEGER);
-        cs.setString(2, input);
-        cs.execute();
-        int rv = cs.getInt(1);
-        if(rv != 0){
-            System.out.println("We don't have this game.");
-            return false;
-        }
-        cs = this.dbService.getConnection().prepareCall("call get_lowest_highest_price (?)");
-        cs.setString(1,input);
+        this.lowest_value = new ArrayList<Float>();
+//        CallableStatement cs = this.dbService.getConnection().prepareCall("call Search_Title(?)");
+////        cs.setString(1, input);
+////        ResultSet rs = cs.executeQuery();
+////        while (rs.next()) {
+////            System.out.println(rs.getString(1));
+////        }
+        CallableStatement cs =this.dbService.getConnection().prepareCall("{call get_lowest_highest_price(?)}");
+        cs.setString(1, input);
         ResultSet rs = cs.executeQuery();
-        while(rs.next()){
-            System.out.println(rs.getString(1));
+        while(rs.next()) {
+            //System.out.println(rs.getFloat(1));
+            this.lowest_value.add(rs.getFloat(1));
         }
+//        if(rv != 0){
+//            System.out.println("We don't have this game.");
+//            return false;
+//        }
+//        cs = this.dbService.getConnection().prepareCall("call get_lowest_highest_price (?)");
+//        cs.setString(1,input);
+//        ResultSet rs = cs.executeQuery();
+//        while(rs.next()){
+//            System.out.println(rs.getString(1));
+//        }
 //        try {
 //            rs = cs.executeQuery();
 //
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -9,7 +10,53 @@ public class Main {
         GameService gs = new GameService(dcs);
         ArrayList<String> output = gs.getGameName();
         System.out.println(output.toString());
-        gs.getLowValue("1");
+        gs.getLowValue("Payday2");
+
+
+        JFrame window = new JFrame("Steam Manager");
+
+
+
+
+        JFrame f;
+        JMenuBar mb;
+        JMenu file,edit,help;
+        JMenuItem cut,copy,paste,selectAll;
+        cut=new JMenuItem("Russia");
+        copy=new JMenuItem("U.S");
+        paste=new JMenuItem("China");
+        selectAll=new JMenuItem("Europe");
+        mb=new JMenuBar();
+        file=new JMenu("Game");
+        edit=new JMenu("Region");
+        help=new JMenu("GameStat");
+        edit.add(cut);edit.add(copy);edit.add(paste);edit.add(selectAll);
+        mb.add(file);mb.add(edit);mb.add(help);
+        window.setJMenuBar(mb);
+//			JButton a = new JButton("Search");
+//			JPanel p1 = new JPanel();
+//
+//			p1.add(a);
+//
+        JPanel p2 = new JPanel();
+        String row[][] = { {"Game","GameStat","Region","CurrentPrice","LowestPrice","HighestPrice"},
+                {"","","","","",""},
+                {"","","","","",""},
+                {"","","","","",""}};
+        String column[]={"","","","","",""};
+        JTable table = new JTable(row,column);
+        p2.add(table);
+//			window.getContentPane().add(p1);
+        window.getContentPane().add(p2);
+
+        window.setSize(500,200);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String gameName = JOptionPane.showInputDialog(window,"Enter the game name");
+        gs.getLowValue(gameName);
+        ArrayList<Float> put = gs.getValue();
+        System.out.println(put.toString());
     }
 
 }
